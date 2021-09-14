20fe8a2a061926f3e321715d2502953bc2601435
MarinaIlina893 <marinailina893@gmail.com>
2021-09-15 01:19:45 +0500

    correct lint fix progression game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,4 +1,4 @@
-from array import *
+import array
 import random
 
 
@@ -7,21 +7,14 @@ def get_description():
 
 
 def generate_task():
-    progression = array('i', )
-    progression.append(random.randint(0, 100))
-    ratio = random.randint(1, 10)
-    progression_size = random.randint(5, 10)
-    hidden_position = random.randint(0, progression_size - 1)
-    i = 1
-    while i < progression_size:
-        progression.append(progression[i-1]+ratio)
-        i += 1
+    progression = generate_progression()
+    hidden_position = random.randint(0, len(progression) - 1)
     task = ''
-    for x in progression:
-        if progression.index(x) != hidden_position:
-            task = task + str(x) + ' '
+    for element in progression:
+        if progression.index(element) == hidden_position:
+            task = '{0}.. '.format(task)
         else:
-            task += '.. '
+            task = '{0}{1} '.format(task, str(element))
     return task.rstrip()
 
 
@@ -29,15 +22,26 @@ def get_answer(task):
     progression = task.split(' ')
     hidden_position = progression.index('..')
     ratio = calculate_ratio(progression, hidden_position)
-    if hidden_position != 0:
-        return str(int(progression[hidden_position-1]) + ratio)
-    else:
-        return str(int(progression[1])-ratio)
+    if hidden_position == 0:
+        return str(int(progression[1]) - ratio)
+    return str(int(progression[hidden_position - 1]) + ratio)
 
 
 def calculate_ratio(progression, hidden_position):
     if hidden_position > 1:
-        return int(progression[1])-int(progression[0])
+        return int(progression[1]) - int(progression[0])
     elif hidden_position == 1:
-        return int(progression[3])-int(progression[2])
+        return int(progression[3]) - int(progression[2])
     return int(progression[2]) - int(progression[1])
+
+
+def generate_progression():
+    progression = array.array('i')
+    progression.append(random.randint(0, 100))
+    ratio = random.randint(1, 10)
+    progression_size = random.randint(5, 10)
+    index = 1
+    while index < progression_size:
+        progression.append(progression[index - 1] + ratio)
+        index += 1
+    return progression
